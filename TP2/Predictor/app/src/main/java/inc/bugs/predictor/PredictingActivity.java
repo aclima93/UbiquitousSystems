36225229daa5f362319150e48a3a30eb695566ac
António Lima <tocalagarto93@gmail.com>
2016-04-19 19:59:14 +0100

    added predicting app project

---
+++ TP2/Predictor/app/src/main/java/inc/bugs/predictor/PredictingActivity.java
@@ -0,0 +1,13 @@
+package inc.bugs.predictor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PredictingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_predicting);
+    }
+}
