3f03889c8c2ab321044d34b6f9b5516e0e69a042
António Lima <tocalagarto93@gmail.com>
2016-05-06 14:31:43 +0100

    added analysis of logs with and without orientations

--- TP2/Matlab/android.m
+++ TP2/Matlab/android.m
@@ -40,4 +40,48 @@ else
     disp('wifi_predictions.txt has no gathered data.');
     
 end
+
+
+% -------------------
+% read data from file
+
+dataset = importdata('wifi_predictions_no_orientation.txt');
+if ~isempty(dataset)
+    
+    % ---------------------------------------------------
+    % parse the dataset from string to translatable codes
+    
+    [expected_y, predicted_y, num_neighbours, var_codes] = parse_predictions(dataset);
+    
+    unique_num_neighbours = unique(num_neighbours);
+    
+    for num = 1:length(unique_num_neighbours)
+        
+        idx = find(num_neighbours == num);
+        expected_y_num_neighbours = expected_y(idx);
+        predicted_y_num_neighbours = predicted_y(idx);
+        
+        used_num_neighbours = var_codes{unique_num_neighbours(num), 2};
+        title_str = sprintf('Android %s Nearest Neighbours', used_num_neighbours);
+        
+        android_accuracy = classification_performance(expected_y_num_neighbours, predicted_y_num_neighbours, title_str, var_codes(:, 1));
+        
+        figure;
+        plot(android_accuracy)
+
+        ax = gca;
+        ax.XTickLabel = var_codes(:, 1);
+        ax.XTickLabelRotation = 90;
+        ax.XTick = 1:length(ax.XTickLabel);
+        title( sprintf('Android Classification Accuracy for %s Nearest Neighbours', used_num_neighbours))
+        xlabel('Class')
+        ylabel('Accuracy')
+        
+    end
+    
+else
+    
+    disp('wifi_predictions_no_orientation.txt has no gathered data.');
+    
+end
 %EOF
--- TP2/Matlab/run_me.m
+++ TP2/Matlab/run_me.m
@@ -1,8 +1,12 @@
 close all;
 clc;
 
-% -------------------
-% read data from file
+% ------------------------------------------
+% Analyse data collected with android device
+run('android');
+
+% -------------------------------------
+% read data from file with orientations
 
 dataset = importdata('wifi_logs.txt');
 if ~isempty(dataset)
@@ -21,11 +25,36 @@ if ~isempty(dataset)
     
     run('decision_tree');
     
-    run('android');
-    
 else
     
     disp('wifi_logs.txt has no gathered data.');
     
 end
+
+% ---------------------------------------
+% read data from file without orientaions
+
+dataset = importdata('wifi_logs_no_orientation.txt');
+if ~isempty(dataset)
+    
+    % ---------------------------------------------------
+    % parse the dataset from string to translatable codes
+    
+    [X, y, var_codes] = parse_data(dataset);
+    
+    % ---------------------------------------------------
+    % split the dataset into training and testing subsets
+    
+    [ train_X, test_X, train_y, test_y ] = split_data( X', y', 0.70 );
+    
+    run('neural_network');
+    
+    run('decision_tree');
+  
+else
+    
+    disp('wifi_logs_no_orientation.txt has no gathered data.');
+    
+end
+
 %EOF
